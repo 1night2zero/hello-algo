8cf3df8f02867df0c3940ba4adf87bf7eb0145be
1night2zero <104056228+1night2zero@users.noreply.github.com>
2024-08-27 03:33:50 -0400

    feat(tree): array binar y tree

--- codes/tree.ts
+++ codes/tree.ts
@@ -112,3 +112,83 @@ console.log("preOrder: ", preOrder(n1));
 console.log("inOrder: ", inOrder(n1));
 console.log("postOrder: ", postOrder(n1));
 console.log("n1: ", n1);
+
+type Order = "pre" | "in" | "post";
+
+/* 数组表示下的二叉树类 */
+class ArrayBinaryTree {
+  #tree: (number | null)[];
+
+  /* 构造方法 */
+  constructor(arr: (number | null)[]) {
+    this.#tree = arr;
+  }
+  /* 列表容量 */
+  size(): number {
+    return this.#tree.length;
+  }
+  /* 获取索引为 i 节点的值 */
+  val(i: number): number | null {
+    // 若索引越界，则返回 null ，代表空位
+    if (i < 0 || i >= this.size()) return null;
+    return this.#tree[i];
+  }
+  /* 获取索引为 i 节点的左子节点的索引 2i + 1 */
+  left(i: number): number {
+    return 2 * i + 1;
+  }
+  /* 获取索引为 i 节点的右子节点的索引 2i + 2 */
+  right(i: number): number {
+    return 2 * i + 2;
+  }
+  /* 获取索引为 i 节点的父节点的索引 p = (i - 1) / 2 */
+  parent(i: number): number {
+    return Math.floor((i - 1) / 2);
+  }
+  /* 层序遍历 */
+  levelOrder(): number[] {
+    let res: number[] = [];
+    // 遍历数组
+    for (let i: number = 0; i < this.size(); i++) {
+      if (this.val(i) !== null) {
+        res.push(this.val(i) as number);
+      }
+    }
+    return res;
+  }
+  /* 深度优先遍历 */
+  #dfs(i: number, order: Order, res: (number | null)[]) {
+    // 若为空位，则返回
+    if (this.val(i) === null) return;
+    // 前序遍历
+    if (order === "pre") res.push(this.val(i));
+    this.#dfs(this.left(i), order, res);
+    // 中序遍历
+    if (order === "in") res.push(this.val(i));
+    this.#dfs(this.right(i), order, res);
+    // 后序遍历
+    if (order === "post") res.push(this.val(i));
+  }
+  /* 前序遍历 */
+  preOrder(): (number | null)[] {
+    const res: number[] = [];
+    this.#dfs(0, "pre", res);
+    return res;
+  }
+
+  /* 中序遍历 */
+  inOrder(): (number | null)[] {
+    const res: number[] = [];
+    this.#dfs(0, "in", res);
+    return res;
+  }
+
+  /* 后序遍历 */
+  postOrder(): (number | null)[] {
+    const res: number[] = [];
+    this.#dfs(0, "post", res);
+    return res;
+  }
+}
+const arrTree = new ArrayBinaryTree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log("arrTree: ", arrTree.levelOrder());
